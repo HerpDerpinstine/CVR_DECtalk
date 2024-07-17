3b848ae448e80867e0890c14fa96345b96fd6d88
Herp Derpinstine <herpderpinstine@gmail.com>
2024-07-17 01:50:39 -0600

    Fixed an issue with Sharing Volation during Native Library extraction

--- Main.cs
+++ Main.cs
@@ -2,6 +2,7 @@
 using CVR_DECtalk.Properties;
 using MelonLoader;
 using MelonLoader.Utils;
+using SharpTalk;
 using System.IO;
 
 namespace CVR_DECtalk
@@ -12,13 +13,14 @@ namespace CVR_DECtalk
         public const string Description = "Adds DECtalk support to Text-To-Speech";
         public const string Author = "Herp Derpinstine";
         public const string Company = "Lava Gang";
-        public const string Version = "1.0.4";
+        public const string Version = "1.0.5";
         public const string DownloadLink = "https://github.com/HerpDerpinstine/CVR_DECtalk";
     }
 
     public class CVR_DECtalk : MelonMod
     {
         internal static MelonLogger.Instance logger;
+        internal static DECtalkEngine engine;
 
         public override void OnInitializeMelon()
         {
@@ -27,6 +29,10 @@ namespace CVR_DECtalk
             WriteFile("DECtalk.dll", Resources.DECtalk_dll);
             WriteFile("dtalk_us.dll", Resources.dtalk_us_dll);
             WriteFile("dtalk_us.dic", Resources.dtalk_us_dic);
+
+            logger.Msg("Initializing DECtalk Engine...");
+            engine = new DECtalkEngine(LanguageCode.None);
+            logger.Msg("DECtalk Engine Initialized!");
 
             MelonLogger.Msg("Adding TTS Module...");
             Comms_TTSHandler.AddModule<TTSModule_DECtalk>("DECtalk", "DECtalk");
@@ -37,9 +43,13 @@ namespace CVR_DECtalk
         {
             MelonLogger.Msg($"Extracting {fileName} from Embedded Resource...");
             string filePath = Path.Combine(MelonEnvironment.GameRootDirectory, fileName);
-            if (File.Exists(filePath))
-                File.Delete(filePath);
-            File.WriteAllBytes(filePath, fileData);
+            try
+            {
+                if (File.Exists(filePath))
+                    File.Delete(filePath);
+                File.WriteAllBytes(filePath, fileData);
+            }
+            catch { }
         }
     }
 }
--- TTSModule_DECtalk.cs
+++ TTSModule_DECtalk.cs
@@ -1,11 +1,6 @@
 ﻿using ABI_RC.Systems.Communications.TTS;
-using MelonLoader;
-using MelonLoader.Utils;
 using SharpTalk;
 using System;
-using System.IO;
-using MelonLoader.TinyJSON;
-using System.Collections.Generic;
 
 namespace CVR_DECtalk
 {
@@ -15,30 +10,26 @@ namespace CVR_DECtalk
         private const int WAV_FOOTER_SIZE = 8;
 		
         private static Type _ttsVoiceType = typeof(TtsVoice);
-        private DECtalkEngine _tts;
 
         public override void Initialize()
         {
             Channels = 1;
             SampleRate = 11025;
 
-            CVR_DECtalk.logger.Msg("Initializing Engine...");
-            _tts = new DECtalkEngine(LanguageCode.None);
-
             CVR_DECtalk.logger.Msg("Loading Voices...");
             foreach (TtsVoice ttsVoice in (TtsVoice[])Enum.GetValues(_ttsVoiceType))
             {
                 string name = Enum.GetName(_ttsVoiceType, ttsVoice);
                 Voices[name] = name;
-		CVR_DECtalk.logger.Msg(name);
+                CVR_DECtalk.logger.Msg(name);
             }
         }
 
         public override short[] Process(string msg)
         {
-            _tts.Voice = (TtsVoice)Enum.Parse(typeof(TtsVoice), CurrentVoice);
+            CVR_DECtalk.engine.Voice = (TtsVoice)Enum.Parse(typeof(TtsVoice), CurrentVoice);
 
-            byte[] memory = _tts.SpeakToMemory(msg, WAVEFORMAT.FORMAT_1M16);
+            byte[] memory = CVR_DECtalk.engine.SpeakToMemory(msg, WAVEFORMAT.FORMAT_1M16);
             if (memory == null)
                 return null;
 
